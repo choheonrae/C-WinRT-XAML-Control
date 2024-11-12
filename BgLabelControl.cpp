f3411dcebaa763b006b9bb128e62897aff06d0c7
choheonrae <127398530+choheonrae@users.noreply.github.com>
2024-11-12 14:23:45 +0900

    Add files via upload

---
+++ BgLabelControl.cpp
@@ -0,0 +1,28 @@
+// BgLabelControl.cpp
+#include "pch.h"
+#include "BgLabelControl.h"
+#if __has_include("BgLabelControl.g.cpp")
+#include "BgLabelControl.g.cpp"
+#endif
+
+namespace winrt::App13::implementation
+{
+    Microsoft::UI::Xaml::DependencyProperty BgLabelControl::m_labelProperty =
+        Microsoft::UI::Xaml::DependencyProperty::Register(
+            L"Label",
+            winrt::xaml_typename<winrt::hstring>(),
+            winrt::xaml_typename<App13::BgLabelControl>(),
+            Microsoft::UI::Xaml::PropertyMetadata{ winrt::box_value(L"default label"), Microsoft::UI::Xaml::PropertyChangedCallback{ &BgLabelControl::OnLabelChanged } }
+        );
+
+    void BgLabelControl::OnLabelChanged(Microsoft::UI::Xaml::DependencyObject const& d, Microsoft::UI::Xaml::DependencyPropertyChangedEventArgs const& /* e */)
+    {
+        if (App13::BgLabelControl theControl{ d.try_as<App13::BgLabelControl>() })
+        {
+            // Call members of the projected type via theControl.
+
+            App13::implementation::BgLabelControl* ptr{ winrt::get_self<App13::implementation::BgLabelControl>(theControl) };
+            // Call members of the implementation type via ptr.
+        }
+    }
+}
